a3ac4f8c63ad7fa016ed83e56e02fb85dd2ae13f
Pavelc4 <pavelc4@users.noreply.github.com>
2026-04-16 01:14:25 +0700

    fix(routes): align POS route paths with test expectations

--- src/routes/pos.ts
+++ src/routes/pos.ts
@@ -7,9 +7,9 @@ const pos = new Hono();
 pos.use("*", authMiddleware);
 
 // POS Session routes
-pos.get("/session/active", POSController.getSession);
-pos.post("/session/open", POSController.openSession);
-pos.post("/session/close", POSController.closeSession);
+pos.get("/session", POSController.getSession);
+pos.post("/open", POSController.openSession);
+pos.post("/close", POSController.closeSession);
 
 // POS Checkout
 pos.post("/checkout", POSController.checkout);
@@ -19,6 +19,6 @@ pos.patch("/transactions/:id", POSController.editTransaction);
 pos.delete("/transactions/:id", POSController.cancelTransaction);
 
 // POS Summary
-pos.get("/summary/today", POSController.getSummaryToday);
+pos.get("/summary-today", POSController.getSummaryToday);
 
 export default pos;
